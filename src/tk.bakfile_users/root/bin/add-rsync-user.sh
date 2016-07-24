681a11b74e0670e5c6cdc87ddb64d50df93b3c5f
jcbrinfo <jcbrinfo@users.noreply.github.com>
2016-07-24 17:52:47 -0400

    add-rsync-users: Refactor
    Signed-off-by: jcbrinfo <jcbrinfo@users.noreply.github.com>

--- src/tk.bakfile_users/root/bin/add-rsync-user.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# Creates the specified rsync user.
-#
-# Usage: `./add-rsync-user.sh {uid} {userName}`
-adduser --disabled-password --gecos "" --uid "$1" "$2" \
-&& adduser "$2" rsync-users
--- src/tk.bakfile_users/root/bin/add-rsync-users.sh
+++ src/tk.bakfile_users/root/bin/add-rsync-users.sh
@@ -3,18 +3,30 @@
 # Creates all rsync users found in `/root/rsync-users`.
 #
 # Each non-empty line of the `/root/rsync-users` file contains a user to add in
-# the `{UID}:{userName}` format.
+# the `<UID>:<user name>` format.
 #
 # Usage: `./add-rsync-users.sh`
+
+set -e
+
+##
+# Creates the specified rsync user.
+#
+# Usage: `add_rsync_user <UID> <user name>`
+add_rsync_user() {
+	adduser --disabled-password --gecos "" --uid "$1" "$2"
+	adduser "$2" rsync-users
+}
+
 while read u; do
 	# Skip empty lines.
 	if [ "${#u}" != 0 ]; then
-		IFS=':' read -a user_array <<< "$u" || exit
+		IFS=':' read -a user_array <<< "$u"
 		if [ "${#user_array[@]}" != 2 ]; then
 			echo "$0"': Unexpected user entry `'"$u"'`.' \
-				'Expected format: `{UID}:{userName}`.' 1>&2
+				'Expected format: `<UID>:<user name>`.' >&2
 			exit 1
 		fi
-	  	/root/bin/add-rsync-user.sh "${user_array[0]}" "${user_array[1]}" || exit
+		add_rsync_user "${user_array[0]}" "${user_array[1]}"
 	fi
 done < /root/rsync-users
