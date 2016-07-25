fd87271cab00ddbb4426fa562bc4c0e9a04271aa
jcbrinfo <jcbrinfo@users.noreply.github.com>
2016-07-25 16:45:27 -0400

    register-ssh-keys: Refactor
    Signed-off-by: jcbrinfo <jcbrinfo@users.noreply.github.com>

--- src/tk.bakfile_data/root/bin/move-ssh-key-file.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-# Moves `{sshKeysPath}` to `/home/{userName}/.ssh/authorized_keys`.
-#
-# Usage: `./move-ssh-key-file.sh {sshKeysPath} {userName}`
-mkdir -p -m 700 "/home/$2/.ssh" \
-&& chmod 600 "$1" \
-&& mv -T "$1" "/home/$2/.ssh/authorized_keys" \
-&& chown "$2:$2" "/home/$2/.ssh" "/home/$2/.ssh/authorized_keys"
--- src/tk.bakfile_data/root/bin/register-ssh-keys.sh
+++ src/tk.bakfile_data/root/bin/register-ssh-keys.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 
 # Move all SSH authentification key list found in `/root/user-keys` to the
 # appropriate directories.
@@ -6,6 +6,19 @@
 # Assume each file to be named after the user that accepts the keys.
 #
 # Usage: `./register-ssh-keys.sh`
+
+set -e
+
+# Moves `<SSH keys path>` to `/home/{user name}/.ssh/authorized_keys`.
+#
+# Usage: `move_key_file {SSH keys path} {user name}`
+move_key_file() {
+	mkdir -p -m 700 "/home/$2/.ssh"
+	chmod 600 "$1"
+	mv -T "$1" "/home/$2/.ssh/authorized_keys"
+	chown "$2:$2" "/home/$2/.ssh" "/home/$2/.ssh/authorized_keys"
+}
+
 for key_file in /root/ssh-auth-keys/*; do
-   /root/bin/move-ssh-key-file.sh "$key_file" "${key_file##*/}" || exit
+   move_key_file "$key_file" "${key_file##*/}"
 done
